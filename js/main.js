57c287ecce584e1b688b903c162857b4cd0a535a
OlehFliurkevych <oleh0809@gmail.com>
2018-03-18 13:04:03 +0200

    Commit

--- js/main.js
+++ js/main.js
@@ -1,7 +1,9 @@
 $('.sl').slick({
-  dots: true,
-  infinite: true,
-  speed: 500,
-  fade: true,
-  cssEase: 'linear'
+    infinite: true,
+    speed: 500,
+    autoplay: true,
+    autoplaySpeed: 3000,
+    fade: true,
+    dots: false,
+    cssEase: 'linear'
 });
